ad5fd658fcd22b668122023654564b50dded1630
Bram Pieters <s1110012@student.windesheim.nl>
2019-10-15 14:33:34 +0200

    Add project files.

---
+++ VrpdScanner/VrpdScanner/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VrpdScanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //InitializeComponent();
+
+            Button b = new Button() { Text = "Open scan page" };
+            b.Clicked += async (sender, e) =>
+            {
+                await Current.MainPage.Navigation.PushAsync(new ScanPage());
+            };
+            // The root page of your application
+            ContentPage content = new ContentPage
+            {
+                Title = "TestZXing",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }, b
+                    }
+                }
+            };
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+    }
+}
---
+++ VrpdScanner/VrpdScanner/Requestor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using RestSharp;
+
+namespace VrpdScanner
+{
+    public static class Requestor
+    {
+        private const string url = "https://vrpd-webapp.azurewebsites.net/api/Guests";
+
+        public static IRestResponse Send(string key)
+        {
+            RestClient client = new RestClient(url);
+            IRestResponse res = client.Execute(new RestRequest(Method.POST).AddJsonBody(key));
+            return res;
+        }
+    }
+}
